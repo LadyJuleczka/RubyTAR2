61ffc0ea4e4ab24902c8694285133c20ff9f0d4d
gwiazdasmierci <julianna.mikucka@gmail.com>
2017-01-09 06:31:58 +0100

    new tests

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -4,8 +4,14 @@ describe Author do
   let(:author) { Author.new :author_name, :author_surname, :yob, :yod, :id }
 
   describe 'initialize' do
-    context 'initializes without errors' do
+    context 'create new object without errors' do
       it { expect(author).to be_an_instance_of(Author) }
+    end
+    it 'initializes without errors' do
+      expect { Author.new('Jan', 'Brzechwa', 1898, 1966, 2) }.not_to raise_error
+    end
+    it 'initializes without errors' do
+      expect { Author.new(1898, 1966, 2) }.to raise_error
     end
     context 'returns name properly' do
       it { expect(author.author_name).to eq :author_name }
