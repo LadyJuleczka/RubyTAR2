d2ccf17cbef2ba4d3ca94a97d4cbe8f0d2d4eaba
gwiazdasmierci <julianna.mikucka@gmail.com>
2017-01-09 04:57:52 +0100

    addind serching books and authors

--- lib/program.rb
+++ lib/program.rb
@@ -27,6 +27,45 @@ while key == 1
     print.authors_to_s(authors, books)
     gets.chop
     menu.clear
+  when '3'
+    menu.clear
+    print 'Wyszukaj: '
+    input = gets.chop
+    while input.length < 3 || input == 'x'
+      break if input == 'x'
+      print 'Podaj przynajmniej trzy znaki (x - anulowanie wyszukania)'
+      input = gets.chop
+    end
+    found = 0
+    books.each do |b|
+      if b.tittle.include?(input)
+        puts b.to2_s
+        found = 1
+      end
+    end
+    puts 'Nie znaleziono zadnej ksiazki spelniajacej kryteria.' if found != 1
+    gets.chop
+    menu.clear
+  when '4'
+    menu.clear
+    print 'Wyszukaj: '
+    input = gets.chop
+    while input.length < 3 || input == 'x'
+      break if input == 'x'
+      print 'Podaj przynajmniej trzy znaki (x - anulowanie wyszukania)'
+      input = gets.chop
+    end
+    found = 0
+    authors.each do |a|
+      if a.author_name.include?(input) || a.author_surname.include?(input)
+        puts a.to_s
+        puts a.books_to_s(books)
+        found = 1
+      end
+    end
+    puts 'Nie znaleziono zadnej ksiazki spelniajacej kryteria.' if found != 1
+    gets.chop
+    menu.clear
   when '0'
     puts 'Nastepuje zamkniecie programu'
     key = 0
