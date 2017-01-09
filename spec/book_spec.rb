a8cf237bf9cf7779d181801528c9da81a77f129b
gwiazdasmierci <julianna.mikucka@gmail.com>
2017-01-09 06:15:32 +0100

    more test of book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,8 +8,30 @@ describe Book do
     context 'initializes without errors' do
       it { expect(book).to be_an_instance_of(Book) }
     end
+    context 'when returns tittle' do
+      it { expect(book.tittle).to eq :tittle }
+    end
     context 'when returns id' do
       it { expect(book.id).to eq :id }
+    end
+    context 'when returns year of realase' do
+      it { expect(book.yor).to eq :yor }
+    end
+    context 'when returns quantity' do
+      it { expect(book.quantity).to eq :quantity }
+    end
+    context 'when returns ISBN' do
+      it { expect(book.isbn).to eq :isbn }
+    end
+  end
+  describe '.get_author' do
+    context 'return type should be an array' do
+      it { expect(book.get_authors).to be_an(Array) }
+    end
+  end
+  describe '.to2_s' do
+    context 'return type should be string' do
+      it { expect(book.to2_s).to be_an(String) }
     end
   end
 end
