976c3d773f20a26cede8a83623d5146c20e63afd
gwiazdasmierci <julianna.mikucka@gmail.com>
2017-01-09 01:05:37 +0100

    changes in book and author

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,13 @@
 # Implementation of Book author
 class Author
-  attr_accessor :author_name, :author_surname, :yob, :yod
+  attr_accessor :author_name, :author_surname, :yob, :yod, :id
 
-  def initialize(author_name, author_surname, yob, yod)
+  def initialize(author_name, author_surname, yob, yod, id)
     @author_name = author_name
     @author_surname = author_surname
     @yob = yob
     @yod = yod
+    @id = id
   end
 
   def to_s
@@ -16,5 +17,15 @@ class Author
     author << 'Rok smierci: ' << yod.to_s << "\n"
     author << 'Wiek: '
     author << (yod.to_s.to_i - yob.to_s.to_i).to_s << "\n"
+  end
+
+  def books_to_s(books)
+    authors_book = 'Ksiazki          : '
+    authors.each do |b|
+      b.authors.each do |a|
+        authors_book << b.tittle.to_s << ', ' if a.id.eql?(id)
+      end
+    end
+    authors_book << "\n\n"
   end
 end
--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,10 @@
 # Implementation of Book class
 class Book
-  attr_accessor :tittle, :author_surname, :yor, :quantity, :isbn
+  attr_accessor :tittle, :id, :yor, :quantity, :isbn
 
-  def initialize(tittle, author_surname, yor, quantity, isbn)
+  def initialize(tittle, id, yor, quantity, isbn)
     @tittle = tittle
-    @author_surname = author_surname
+    @id = id
     @yor = yor
     @quantity = quantity
     @isbn = isbn
@@ -12,13 +12,19 @@ class Book
 
   def to_s
     book = 'Tytul:    : ' << tittle.to_s << "\n"
-    book << 'Nazwisko autora: ' << author_surname.to_s << "\n"
     book << 'Rok Wydania: ' << yor.to_s << "\n"
     book << 'Ilosc : ' << quantity.to_s << "\n"
     book << 'ISBN: ' << isbn.to_s << "\n"
   end
 
   def authors_to_s(authors)
-    books_authors = ''
+    books_authors = 'Autorzy          : '
+    authors.each do |a|
+      a.books.each do |b|
+        books_authors << a.author_name.to_s << ' '
+        books_authors << a.author_surname.to_s << ', ' if b.id.eql?(id)
+      end
+    end
+    books_authors << "\n\n"
   end
 end
