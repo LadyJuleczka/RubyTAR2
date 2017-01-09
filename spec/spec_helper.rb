7380add8250061d375e499a8961159f604b0c391
gwiazdasmierci <julianna.mikucka@gmail.com>
2017-01-09 05:05:03 +0100

    adding spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require 'simplecov'
+
+Simplecov.start
+
+require_relative '../lib/author'
+require_relative '../lib/book'
+require_relative '../lib/to_a'
+require_relative '../lib/print'
+require_relative '../lib/menu'
